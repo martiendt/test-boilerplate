e64524c72c28e5b7f7eb37ed690a28c97bd14619
Martien Dermawan Tanama <martiendt@gmail.com>
2021-08-07 12:41:53 +0700

    add eslint prettier

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  plugins: ["prettier"],
+  extends: ["plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {},
+};
