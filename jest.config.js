b209debee5caa8a20f790b54d5e78d902d93d0f5
Martien Dermawan Tanama <martiendt@gmail.com>
2021-09-15 15:58:28 +0700

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ export default {
   // automock: false,
 
   // Stop running tests after `n` failures
-  // bail: 0,
+  bail: 1,
 
   // The directory where Jest should store its cached dependency information
   // cacheDirectory: "/private/var/folders/mr/ldmy4pl97c71t_4hg886mmk40000gn/T/jest_dx",
@@ -120,9 +120,7 @@ export default {
   // rootDir: undefined,
 
   // A list of paths to directories that Jest should use to search for files in
-  // roots: [
-  //   "<rootDir>"
-  // ],
+  roots: ["src"],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
