aa1f96a6c42467ac9b4a4259a4e25aeab357a142
Martien Dermawan Tanama <martiendt@gmail.com>
2021-10-09 11:07:00 +0700

    add validation unique

--- src/middleware/validation/index.js
+++ src/middleware/validation/index.js
@@ -1,14 +1,34 @@
 import Validator from "validatorjs";
+import Connection from "#src/database/connection.js";
 import ApiError from "#src/middleware/error-handler/api-error.js";
 
 export default function (rules = {}) {
   return function (req, res, next) {
+    Validator.registerAsync("unique", async function (value, attribute, req, passes) {
+      let [table, column] = attribute.split(",");
+
+      if (column === undefined) column = req;
+
+      const count = await Connection.getCollection(table).countDocuments({
+        [column]: value,
+      });
+
+      if (count === 1) {
+        passes(false, `The ${req} field is exists`);
+      } else {
+        passes();
+      }
+    });
+
     const validator = new Validator(req.body, rules);
 
-    if (validator.fails()) {
-      return next(ApiError.unprocessableEntity(undefined, validator.errors));
-    }
-
-    next();
+    validator.checkAsync(
+      () => {
+        next();
+      },
+      () => {
+        return next(ApiError.unprocessableEntity(undefined, validator.errors));
+      }
+    );
   };
 }
